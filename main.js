429d1d7375626ea29e322d8c5890aec04f1253c9
Andy Hall <andersonhall@gmail.com>
2025-05-08 07:47:22 -0500

    play game logic

--- main.js
+++ main.js
@@ -21,3 +21,30 @@ const getHumanChoice = () => {
     return "scissors";
   }
 };
+
+const playGame = (numberOfRoundsToPlay) => {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  const playRound = (humanChoice, computerChoice) => {
+    if (
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "paper" && computerChoice === "rock") ||
+      (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+      console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+      humanScore++;
+    } else if (humanChoice === computerChoice) {
+      console.log(`It's a tie!`);
+    } else {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+      computerScore++;
+    }
+  };
+
+  for (let i = 0; i < numberOfRoundsToPlay; i++) {
+    playRound(getHumanChoice(), getComputerChoice());
+  }
+};
+
+playGame(5);
